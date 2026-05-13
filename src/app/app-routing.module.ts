4ccd2743b8ee16997071a505693f8c6c20a53a64
donia abozeid <doniaabozeid16@gmail.com>
2026-05-13 15:53:55 +0200

    Refactor code structure for improved readability and maintainability

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GameSetupComponent } from './pages/game-setup/game-setup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: GameSetupComponent },
+  { path: 'setup', component: GameSetupComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameSetupComponent } from './pages/game-setup/game-setup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameSetupComponent
   ],
   imports: [
     BrowserModule,
